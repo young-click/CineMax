5d2d036e72ebee4263b25eff3e27396f4714a45d
young-click <vaibhavpangare61@gmail.com>
2023-10-21 16:05:17 +0530

    final touchup

--- src/components/Header.js
+++ src/components/Header.js
@@ -30,7 +30,7 @@ export const Header = () => {
   }
 
   return (
-    <header className="fixed w-screen">      
+    <header>      
       <nav className="bg-white border-b-2 border-gray-200 px-2 sm:px-4 py-2 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900">
         <div className="flex flex-wrap justify-between items-center mx-auto">
           <Link to="/" className="flex items-center">
